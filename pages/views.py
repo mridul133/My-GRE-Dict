e517a53841a9ac67875b542dc099da3124083fae
mridul133 <mridul2013331015@gmail.com>
2021-05-01 21:38:04 +0600

    Refactored views.py

--- pages/views.py
+++ pages/views.py
@@ -1,59 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import json
-import os
-from pages.models import Word
-from pages.models import Global
+from pages.models import *
 from django.contrib import messages 
 from django.db.models import F
 import random
+import json
+import os
+
 
 MX_WEIGHT = 100.0
 
-
 def home_view(request, *args, **kwargs):
 
-    print("---- 1 -----_> ", request.GET)
-    print("---- 2 -----_> ", request.POST)
-        
+    prev_word = update_info_about_prev_word(request)
+    next_word = get_next_word(request, prev_word)
 
-    old_word = ""
-    if('word' in request.POST):
-        update_info_about_old_word(request)
-        old_word = request.POST['word']
-
-    isSearched = 0
-    if('search' in request.GET):
-        isSearched = 1
-        old_word = request.GET['search']
-
-    word = get_next_word(old_word, isSearched)
-
-    global_var = Global.objects.all()
-    all_words = Word.objects.all()
-    
-    ret = {}
-
-    ret['word'] = word[0]
-    ret['pos'] = "("+word[1]+")"
-    ret['def'] = word[2]
-    ret['exm'] = word[3]
-    ret['weight'] = word[4]
-    ret['cnt'] = word[5]
-    ret['alpha_sort'] = global_var[0].AlphaSort
-    ret['mastered'] = global_var[0].MasteredCnt
-    ret['total'] = global_var[0].TotalWords
-    ret['mx_weight'] = MX_WEIGHT
-
-    lst = []
-    for w in all_words:
-        lst.append(str(w.Word))
-
-    json_list = json.dumps(lst)
-
-    ret['all_words'] = json_list
-
-    # print("ret ----- >", ret)
+    ret = get_return_object(next_word)
 
     return render(request, "home.html", ret)
 
@@ -63,6 +25,139 @@ def manage_view(request, *args, **kwargs):
 
 
 #==================================== Helper Functions ===================================
+
+
+def update_info_about_prev_word(request):
+
+    if('word' not in request.POST):
+        return ""
+    
+    prev_word = request.POST['word']
+    prev_word_weight = float(request.POST['weight'])
+    
+    is_sort_order_alpha = 0
+
+    if('sort_order' in request.POST):
+        if(request.POST['sort_order'] == "on"):
+            is_sort_order_alpha = 1
+        else:
+            is_sort_order_alpha = 0
+    else:
+        is_sort_order_alpha = 0
+        
+
+    Global.objects.all().update(AlphaSort = is_sort_order_alpha)
+    Global.objects.all().update(TotalWords = len(Word.objects.all()))
+
+    Word.objects.filter(Weight__gt = MX_WEIGHT/5.0).update(Weight = F("Weight") + 1)
+    Word.objects.filter(Weight__lte = MX_WEIGHT/5.0).update(Weight = F("Weight") + 0.3)
+    Word.objects.filter(Weight__gt = MX_WEIGHT).update(Weight = MX_WEIGHT)
+
+    db_entry =  Word.objects.filter(Word = prev_word)
+
+    if(len(db_entry) == 0):
+        return ""
+
+    db_entry[0].AppearCnt += 1
+
+    if(abs(prev_word_weight-db_entry[0].Weight) >= 1.0):
+        db_entry[0].Weight = prev_word_weight
+    db_entry[0].save()
+
+    Global.objects.all().update(MasteredCnt = len(Word.objects.filter(Weight__lte = MX_WEIGHT/5.0)))
+    
+    return prev_word
+
+
+def get_next_word(request, prev_word):
+
+    isSearched = 0
+    if('search' in request.GET):
+        isSearched = 1
+        prev_word = request.GET['search']
+    
+    all_words = Word.objects.all()
+
+    if(len(Global.objects.all()) == 0):
+        Global.objects.create(TotalWords = len(all_words), MasteredCnt = 0, AlphaSort = 0)
+
+    if(len(all_words) == 0):
+        ret = ["No word in the database", "", "", "", 0.0, 0]
+        return ret
+
+    if(len(all_words) > 1):
+        if(isSearched == 1):
+            all_words = all_words.filter(Word = prev_word)
+        else:
+            all_words = all_words.exclude(Word = prev_word)
+
+    words = []
+
+    for word in all_words:
+        words.append([word.Word, word.POS, word.Definition, word.Example, word.Weight, word.AppearCnt])
+
+    if(len(Global.objects.filter(AlphaSort = 1)) > 0):
+        return get_next_word_in_alphabatical_order(words)
+    
+    return get_next_high_weight_random_word(words)
+
+
+def get_next_word_in_alphabatical_order(words):
+
+    words.sort(key=lambda tup: tup[0])
+    ind = 0
+    while(ind < len(words)):
+        if(words[ind][4] > MX_WEIGHT/5.0):
+            break
+        ind += 1
+    if(ind >= len(words)):
+        ind -= 1
+
+    return words[ind]
+
+def get_next_high_weight_random_word(words):
+    
+    words.sort(key=lambda tup: tup[4], reverse=True)
+
+    top_words = []
+    for i in range(0, len(words)):
+        if(words[i][4] == words[0][4]):
+            top_words.append(words[i])
+
+    random.shuffle(top_words)
+
+    return top_words[0]
+
+
+def get_return_object(next_word):
+
+    global_var = Global.objects.all()
+    all_words = Word.objects.all()
+    
+    words = []
+    for word in all_words:
+        words.append(str(word.Word))
+
+    json_words = json.dumps(words)
+
+    ret = {}
+
+    ret['word'] = next_word[0]
+    ret['pos'] = "("+next_word[1]+")"
+    ret['def'] = next_word[2]
+    ret['exm'] = next_word[3]
+    ret['weight'] = next_word[4]
+    ret['cnt'] = next_word[5]
+    ret['alpha_sort'] = global_var[0].AlphaSort
+    ret['mastered'] = global_var[0].MasteredCnt
+    ret['total'] = global_var[0].TotalWords
+    ret['all_words'] = json_words
+    ret['mx_weight'] = MX_WEIGHT
+
+    return ret
+
+
+
 
 def add_new_word(request):
 
@@ -81,6 +176,7 @@ def add_new_word(request):
 def delete_word():
     print("deleted")
 
+
 def initialize_db_with_magoosh1000(request):
     
     Word.objects.all().delete()
@@ -89,9 +185,7 @@ def initialize_db_with_magoosh1000(request):
     with open(os.path.dirname(os.path.realpath(__file__)) + '/magoosh1000.json') as f:
         data = json.load(f)
 
-    cnt = 0
     for curr in data:
-        cnt += 1
 
         curr['word'] = fix_lower_upper(curr['word'])
         curr['pos'] = fix_lower_upper(curr['pos'])
@@ -169,90 +263,3 @@ def fix_lower_upper(s):
     s = ''.join(chars)
 
     return s
-
-
-def get_next_word(old_word, isSearched):
-    
-    all_words = Word.objects.all()
-
-    if(len(Global.objects.all()) == 0):
-        Global.objects.create(TotalWords = len(all_words), MasteredCnt = 0, AlphaSort = 0)
-
-    if(len(all_words) == 0):
-        ret = ["No word in the database", "", "", "", 0.0, 0]
-        return ret
-
-    if(len(all_words) > 1):
-        if(isSearched == 1):
-            all_words = all_words.filter(Word = old_word)
-        else:
-            all_words = all_words.exclude(Word = old_word)
-
-    words = []
-
-    for word in all_words:
-        words.append([word.Word, word.POS, word.Definition, word.Example, word.Weight, word.AppearCnt])
-
-    if(len(Global.objects.filter(AlphaSort = 1)) > 0):
-        words.sort(key=lambda tup: tup[0])
-        ind = 0
-        while(ind < len(words)):
-            if(words[ind][4] > MX_WEIGHT/5.0):
-                break
-            ind += 1
-        if(ind >= len(words)):
-            ind -= 1
-
-        return words[ind]
-        
-
-    words.sort(key=lambda tup: tup[4], reverse=True)
-
-    top_words = []
-    for i in range(0, len(words)):
-        if(words[i][4] == words[0][4]):
-            top_words.append(words[i])
-
-    random.shuffle(top_words)
-
-    ret = top_words[0]
-
-    return ret
-
-def update_info_about_old_word(request):
-
-    old_word = request.POST['word']
-    old_word_weight = float(request.POST['weight'])
-    
-    check_box = 0
-
-    if('sort_order' in request.POST):
-        if(request.POST['sort_order'] == "on"):
-            check_box = 1
-        else:
-            check_box = 0
-    else:
-        check_box = 0
-
-
-    Word.objects.filter(Weight__gt = MX_WEIGHT/5.0).update(Weight = F("Weight") + 1)
-    Word.objects.filter(Weight__lte = MX_WEIGHT/5.0).update(Weight = F("Weight") + 0.3)
-    Word.objects.filter(Weight__gt = MX_WEIGHT).update(Weight = MX_WEIGHT)
-
-    Global.objects.all().update(AlphaSort = check_box)
-
-    Global.objects.all().update(TotalWords = len(Word.objects.all()))
-
-    db_entry =  Word.objects.filter(Word = old_word)
-
-    if(len(db_entry) == 0):
-        return
-
-    db_entry[0].AppearCnt += 1
-
-    if(abs(old_word_weight-db_entry[0].Weight) >= 1.0):
-        db_entry[0].Weight = old_word_weight
-    db_entry[0].save()
-
-    Global.objects.all().update(MasteredCnt = len(Word.objects.filter(Weight__lte = MX_WEIGHT/5.0)))
-    
